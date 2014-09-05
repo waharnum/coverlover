e1e9b695021d01eadca610936306e4172bfc0726
Alan Harnum <Alan Harnum>
2014-09-05 06:15:26 -0400

    Use HEAD request and check content-length rather than checking image at all.

--- syndetics_source.rb
+++ syndetics_source.rb
@@ -1,7 +1,7 @@
-require 'open-uri'
-require 'RMagick'
+require './cover_info'
+require './cover_source'
 require 'date'
-include Magick
+require 'net/http'
 
 class SyndeticsSource < CoverSource
 
@@ -10,30 +10,32 @@ class SyndeticsSource < CoverSource
 	end	
 
 	def check_cover(isbn)		
-		syndetics_url = "http://syndetics.com/index.aspx?isbn=#{isbn}/SC.gif"
-		puts "Checking image in #{@name} at #{syndetics_url}"
-		syndetics_file = "#{isbn}-SC.gif";
-		open(syndetics_url) {|f|			
-			File.open("#{syndetics_file}", "w") do |file|
-				file.puts f.read
-			end
-		}
+		syndetics_base = "syndetics.com"
+		syndetics_path = "/index.aspx?isbn=#{isbn}/SC.gif"
+		puts "Checking image in #{@name} at #{syndetics_base}#{syndetics_path}"
+		content_length = 0
+
+		Net::HTTP.start(syndetics_base) do |http|
+		  http.open_timeout = 2
+		  http.read_timeout = 2
+		  content_length = http.head(syndetics_path)["content-length"]		  
+		end
+
+		puts content_length
 
 		ci = CoverInfo.new("Syndetics")
 		ci.last_checked = Date.today.to_s
-		ci.url = syndetics_url	
+		ci.url = syndetics_base+syndetics_path
 		ci.isbn = isbn.to_s		
-
-		lc = ImageList.new(syndetics_file)
-		if(lc.columns > 1)
+		
+		
+		if(content_length.to_i > 100)
 			puts "Syndetics has a cover image!"
 			ci.has_cover = true
 		else
 			puts "Syndetics has no cover image!"
 			ci.has_cover = false
 		end		
-
-		File.delete(syndetics_file)
 
 		return ci				
 	end
