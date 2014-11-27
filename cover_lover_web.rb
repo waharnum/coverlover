de991b9ececd7a50f7c9fe93566649d2c981b1da
Alan Harnum <waharnum@gmail.com>
2014-11-27 07:47:30 -0500

    build out basics of web front end, add more methods to mongo client

--- cover_lover_web.rb
+++ cover_lover_web.rb
@@ -5,10 +5,24 @@ require './open_library_source'
 require './cover_info'
 require './cover_mongo'
 require 'sinatra'
+require 'mongo'
+require 'json'
+include Mongo
 
 get '/cover/isbn/:isbn' do
   isbn = params[:isbn]  
-  cover = Cover.new(isbn,"")  
-  cover.check_sources()
-  "#{cover.to_json}"
+
+  if(CoverMongo.has_isbn(isbn))
+  	"
+  	<h1>Cover Located in Mongo</h1>
+  	#{CoverMongo.get_isbn(isbn)}
+  	"
+  else
+  	cover = Cover.new(isbn,"")  
+  	cover.check_sources()
+  	"<h1>Cover Not Located in Mongo</h1>
+  	#{cover.to_json}
+  	Information stored in Mongo with ID#{CoverMongo.store_cover(cover)}"
+  end
+
 end
--- cover_mongo.rb
+++ cover_mongo.rb
@@ -1,13 +1,31 @@
 class CoverMongo
-	def self.has_isbn(isbn)
+
+	def self.get_cover_collection()
 		client = MongoClient.new("localhost",27017)
 		db = client.db("coverdb")
 		coll = db.collection("covercollection")
+		return coll
+	end
+
+	def self.has_isbn(isbn)
+		coll = get_cover_collection()
 		matches = coll.find({'data.isbn' => isbn})		
 		if(matches.count > 0) 
 			return true
 		else 
 			return false
 		end
+	end
+
+	def self.get_isbn(isbn)
+		coll = get_cover_collection()
+		matches = coll.find_one({'data.isbn' => isbn})	
+		return matches
+	end
+
+	def self.store_cover(cover)
+		coll = get_cover_collection()
+		id = coll.insert(JSON.parse(cover.to_json()))
+		return id
 	end
 end
